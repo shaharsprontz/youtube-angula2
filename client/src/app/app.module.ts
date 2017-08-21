eaf95597fe7fe84f55a8574b6ed08fd7c16822eb
shahar-sprontz <shahar@somoto.net>
2017-08-21 17:53:56 +0300

    added register route and form go to the 22nd minute on the tut to continue with emailValidation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
@@ -19,7 +19,7 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  createForm(){
+    this.form = this.FormBuilder.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30)
+      ])],
+      username: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(15)
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(35)
+      ])],
+      confirm: ['', Validators.required]
+    })
+  }
+
+onRegisterSubmit() {
+  console.log('form submitted');
+}
+
+
+
+  constructor(
+    private FormBuilder: FormBuilder
+  ) {
+    this.createForm()
+   }
 
   ngOnInit() {
   }
