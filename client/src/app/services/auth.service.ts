4c7cb5d19dfcdbb17b7ef3e7c87ae0bde2671652
shaharsprontz <sh.sprontz@gmail.com>
2017-09-02 21:15:49 +0300

    finished register form and login form + session and token

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/map';
 export class AuthService {
 
 domain = "http://localhost:8080";
+authToken;
+user;
 
   constructor(
     private http: Http
@@ -22,4 +24,15 @@ checkUsername(username) {
 checkEmail(email) {
   return this.http.get(this.domain + '/authentication/checkEmail/' + email).map(res => res.json());
   }
+
+login(user) {
+  return this.http.post(this.domain + '/authentication/login', user).map(res => res.json());
+}
+
+storeUserDate(token, user) {
+  localStorage.setItem('token', token);
+  localStorage.setItem('user', JSON.stringify(user))
+  this.authToken = token;
+  this.user = user;
+}
 }
