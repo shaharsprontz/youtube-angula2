1b192ddafe5a7343569892e38cef6bb58d610834
shahar-sprontz <shahar@somoto.net>
2017-10-26 18:58:37 +0300

    Continue with user token

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,42 +1,64 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  Http,
+  Headers,
+  RequestOptions
+} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
 
-domain = "http://localhost:8080";
-authToken;
-user;
+  domain = "http://localhost:8080";
+  authToken;
+  user;
+  options;
 
   constructor(
     private http: Http
-  ) { }
-    
-registerUser(user) {
-  return this.http.post(this.domain + '/authentication/register', user).map(res => res.json());
+  ) {}
+
+  createAutenticationHeaders() {
+    this.loadToken();
+    this.options = new RequestOptions({
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'authorization': this.authToken
+      })
+    })
   }
 
-checkUsername(username) {
-  return this.http.get(this.domain + '/authentication/checkUsername/' + username).map(res => res.json());
+  loadToken() {
+    const token = localStorage.getItem('token');
+    this.authToken = token;
   }
 
-checkEmail(email) {
-  return this.http.get(this.domain + '/authentication/checkEmail/' + email).map(res => res.json());
+  registerUser(user) {
+    return this.http.post(this.domain + '/authentication/register', user).map(res => res.json());
   }
 
-login(user) {
-  return this.http.post(this.domain + '/authentication/login', user).map(res => res.json());
-}
+  checkUsername(username) {
+    return this.http.get(this.domain + '/authentication/checkUsername/' + username).map(res => res.json());
+  }
 
-storeUserDate(token, user) {
-  localStorage.setItem('token', token);
-  localStorage.setItem('user', JSON.stringify(user))
-  this.authToken = token;
-  this.user = user;
-}
+  checkEmail(email) {
+    return this.http.get(this.domain + '/authentication/checkEmail/' + email).map(res => res.json());
+  }
 
-getProfile() {
-  
-}
+  login(user) {
+    return this.http.post(this.domain + '/authentication/login', user).map(res => res.json());
+  }
+
+  storeUserDate(token, user) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user))
+    this.authToken = token;
+    this.user = user;
+  }
+
+  getProfile() {
+    this.createAutenticationHeaders()
+  }
 }
